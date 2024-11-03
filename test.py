1693922d913ceae9d77f596a777571bafdb06c53
阳 小 <mysteriousYang@qq.com>
2024-11-03 19:12:27 +0800

    remove some files

--- test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import numpy as np
-
-if __name__ == "__main__":
-    a = np.array([]).reshape(0,3)
-    a = np.vstack((a,[1,2,3]))
-    a = np.vstack((a,[6,7,8]))
-    print(a)
-    pass
